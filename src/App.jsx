967b7f06bdba98293415be38d7e1c6c323990eab
Musbahniar-MNR <ruang.9campus@gmail.com>
2019-01-17 14:38:58 +0700

    second upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './App.css';
+import Clock from './Clock';
+
+class App extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            deadline: 'December 24, 2019',
+            newDeadLine: ''
+        }
+    }
+
+    editDeadLine () {
+        this.setState ({deadline: this.state.newDeadLine})
+    }
+
+    render() {
+    return (
+      <div className='App'>
+            <div className='App-title'>Countdown to {this.state.deadline}</div>
+            <Clock deadline={this.state.deadline}/>
+            <div>
+                <input 
+                    placeholder='new date' 
+                    onChange={event => this.setState({newDeadLine: event.target.value})}
+                />
+                <button onClick={()=> this.editDeadLine()}>Submit</button>
+            </div>
+      </div>
+    )
+  }
+}
+
+export default App;
